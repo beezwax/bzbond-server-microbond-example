04ee71758b85896aaffa791b153a7afabdff145e
Alec Gregory <alec_g@beezwax.net>
2024-04-29 14:08:25 +0100

    Restructure

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ module.exports = {
   },
   options: {},
 };
+
+async function helloWorld(fastify, options) {
+  fastify.get(
+    "/bzmb-hello-world",
+    async (req, res) => {
+      try {
+        const greeting = await helloWorld();
+        res
+          .code(200)
+          .send(greeting);
+      } catch (error) {
+        res
+          .code(500)
+          .send(error);
+      }
+    }
+  )
+}
+
+module.exports = { microbond: helloWorld };
